fc06bda35b9d053675487619c7e4faee6b94e5d2
bladerunner2020 <pivovarov@gmail.com>
2019-01-10 15:31:02 +0300

    Added animation delay option, freeze label item (no movement), fixed bug with x-coordinates

--- index.js
+++ index.js
@@ -20,10 +20,21 @@ function SmartSlider(item) {        // eslint-disable-line no-unused-vars
     /**
      * Set value label item that is moved together with slider
      * @param {object} item - value label item 
+     * @param {boolean} [freeze] - freeze movement of value item
      */
-    this.setValueItem = function(item) {
+    this.setValueItem = function(item, freeze) {
         this.valueLabel = item;
         this.valueLabelOffsetX = this.valueLabel.X - this.slider.Width - this.slider.X;
+        this.freezedValueItem = freeze;
+        return this;
+    };
+
+    /**
+     * Freeze movement of value item
+     * @param {boolean} freeze
+     */
+    this.freezeValueItem = function(freeze) {
+        this.freezedValueItem = freeze;
         return this;
     };
 
@@ -32,7 +43,7 @@ function SmartSlider(item) {        // eslint-disable-line no-unused-vars
      * @param {number} min - minimal X to move value label, if undefined - no limit
      * @param {number} max - maximal X to move value label, if undefined - no limit
      */
-    this.setLabelLimits = function(min, max) {
+    this.setValueItemLimits = function(min, max) {
         this.minX = min;
         this.maxX = max;
         return this;
@@ -53,14 +64,16 @@ function SmartSlider(item) {        // eslint-disable-line no-unused-vars
      * @param {number} time - time in ms that is required for animation the entire slider to 100%
      *                        factual time depends on the slider value - if slider value is min then 
      *                        there is no animation at all
-     * @param {number} type - type of formula calculation (see IR.Tween), IR.TWEEN_LINEAR - default
+     * @param {number} [type] - type of formula calculation (see IR.Tween), IR.TWEEN_LINEAR - default
+     * @param {number} [delay] - delay in ms to start animation
      */
-    this.setAnimation = function(time, type) {
+    this.setAnimation = function(time, type, delay) {
         if (type == undefined) {
             type = IR.TWEEN_LINEAR;
         }
         this.animationTime = time;
         this.animationType = type;
+        this.animationDelay = delay;
         return this;
     };
 
@@ -111,27 +124,29 @@ function SmartSlider(item) {        // eslint-disable-line no-unused-vars
      * Update value item, value item position and slider color
      */
     this.update = function() {
-        var x = this.slider.X;
-
-        var width = this.slider.Width;
         var value = this.slider.Value;
         var maxValue = this.slider.Max;  
         var minValue = this.slider.Min;   
 
-        var newX = x + Math.floor(width/maxValue * value);
-        newX += that.valueLabelOffsetX;
-
-        if (this.minX) {
-            newX = (newX > this.minX) ? newX : this.minX;
-        }
-
-        if (this.maxX) {
-            newX = (newX < this.maxX) ? newX : this.maxX;
-        }
-         
         if (that.valueLabel) {
             this.valueLabel.Value = value;
-            this.valueLabel.X = newX;
+            if (!this.freezedValueItem) {
+                var x = this.slider.X;
+                var width = this.slider.Width;
+                var delta = (maxValue <= minValue) ? width : Math.floor(width/(maxValue - minValue) * (value - minValue));
+                var newX = x + delta;
+                newX += that.valueLabelOffsetX;
+    
+                if (this.minX) {
+                    newX = (newX > this.minX) ? newX : this.minX;
+                }
+    
+                if (this.maxX) {
+                    newX = (newX < this.maxX) ? newX : this.maxX;
+                }
+   
+                this.valueLabel.X = newX;
+            }
         }
 
         if (this.gradient) {
@@ -151,11 +166,12 @@ function SmartSlider(item) {        // eslint-disable-line no-unused-vars
      * from Min to the current value
      * @param {number} [newValue] - if newValue is not set then animate to the current value
      * @param {number} [startValue] - if startValue is not set min value is used
+     * @param {boolean} [noDelay] - force animation without update even if animationDelay is set
      */
-    this.updateX = function(newValue, startValue) {
+    this.updateX = function(newValue, startValue, noDelay) {
         if (newValue == undefined) { newValue = this.slider.Value; }
         if (startValue == undefined) { startValue = this.slider.Min; }
-        if (this.animationTime == undefined) { 
+        if (!this.animationTime) { 
             this.setValue(newValue);
             return; 
         }
@@ -165,7 +181,7 @@ function SmartSlider(item) {        // eslint-disable-line no-unused-vars
         animationData.endValue = newValue;
         animationData.duration = this.animationTime;
 
-        this.startAnimation(animationData);
+        this.startAnimation(animationData, noDelay ? null : this.animationDelay);
     };
 
     /**
@@ -176,16 +192,8 @@ function SmartSlider(item) {        // eslint-disable-line no-unused-vars
         var slider = this.slider;
         var animationData = this.animationData;
         animationData.current += elapsed;
-        var duration = animationData.duration;
-        var value = 0;
-
-        if (animationData.endValue > animationData.startValue) {
-            value = IR.Tween(slider.animationType, animationData.current, 0, animationData.endValue - animationData.startValue, duration);       
-            value += animationData.startValue;
-        } else {
-            value = IR.Tween(slider.animationType, animationData.current, 0, animationData.startValue - animationData.endValue, duration); 
-            value = animationData.startValue - value;
-        }
+        var value = IR.Tween(slider.animationType, animationData.current, animationData.startValue, 
+            (animationData.endValue - animationData.startValue), animationData.duration);       
 
         if (animationData.current >= animationData.duration) {
             slider.stopAnimation();
@@ -199,7 +207,15 @@ function SmartSlider(item) {        // eslint-disable-line no-unused-vars
      * Start animation
      * @param {object} animationData
      */
-    this.startAnimation = function(animationData) {
+    this.startAnimation = function(animationData, delay) {
+        if (delay) {
+            IR.SetTimeout(delay, function() {
+                that.startAnimation(animationData);
+            });
+            return;
+        }
+
+
         if (this.animationActive) { this.stopAnimation(); }
 
         animationData.current = 0;
@@ -219,7 +235,11 @@ function SmartSlider(item) {        // eslint-disable-line no-unused-vars
     IR.AddListener(IR.EVENT_ITEM_PRESS, this.slider, this.update, this);
     IR.AddListener(IR.EVENT_TOUCH_DOWN, this.slider, this.update, this);
     IR.AddListener(IR.EVENT_TOUCH_MOVE, this.slider, this.update, this);
-    IR.AddListener(IR.EVENT_ITEM_SHOW, this.slider, function() { this.updateX(); }, this);
+    IR.AddListener(IR.EVENT_ITEM_SHOW, this.slider, function() { 
+        var value = this.slider.Value;
+        this.slider.Value = this.slider.Min;
+        this.updateX(value); 
+    }, this);
 }
 
 
